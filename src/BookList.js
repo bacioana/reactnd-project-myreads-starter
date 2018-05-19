fa29dad1676abbdb3b7d5baac21bed6c3a2f97a9
Ioana Bac <baacioana@gmail.com>
2018-05-19 11:46:07 +0300

    Created BookList component

---
+++ src/BookList.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+import {Link} from 'react-router-dom'
+
+class BookList extends Component {
+	state = {
+	    /**
+	     * TODO: Instead of using this state variable to keep track of which page
+	     * we're on, use the URL in the browser's address bar. This will ensure that
+	     * users can use the browser's back and forward buttons to navigate between
+	     * pages, as well as provide a good URL they can bookmark and share.
+	     */
+	    showSearchPage: false
+  	}
+
+	render() {
+		return(
+			<div className="app">
+		        {this.state.showSearchPage ? (
+		          <div className="search-books">
+		            <div className="search-books-bar">
+		              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+		              <div className="search-books-input-wrapper">
+		                {/*
+		                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+		                  You can find these search terms here:
+		                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+		                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+		                  you don't find a specific author or title. Every search is limited by search terms.
+		                */}
+		                <input type="text" placeholder="Search by title or author"/>
+
+		              </div>
+		            </div>
+		            <div className="search-books-results">
+		              <ol className="books-grid"></ol>
+		            </div>
+		          </div>
+		        ) : (
+		          	<div className="list-books">
+			            <div className="list-books-title">
+			              <h1>MyReads</h1>
+			            </div>
+			            <div className="list-books-content">
+			              <div>
+			                <div className="bookshelf">
+			                  <h2 className="bookshelf-title">{this.props.shelf}</h2>
+			                  <div className="bookshelf-books">
+			                    <ol className="books-grid">
+			                    	{this.props.books.map((book) => (
+			                    		<li key={book.title}>
+					                        <div className="book">
+					                          <div className="book-top">
+					                            <div className="book-cover" style={{width: 128, height: 193, backgroundImage:`url(${book.url})` }}></div>
+					                            <div className="book-shelf-changer">
+					                              <select>
+					                                <option value="none" disabled>Move to...</option>
+					                                <option value="currentlyReading">Currently Reading</option>
+					                                <option value="wantToRead">Want to Read</option>
+					                                <option value="read">Read</option>
+					                                <option value="none">None</option>
+					                              </select>
+					                            </div>
+					                          </div>
+					                          <div className="book-title">{book.title}</div>
+					                          <div className="book-authors">{book.authors}</div>
+					                        </div>
+					                    </li>
+			                    	))}		                      
+			                    </ol>
+			                  </div>
+			                </div>		              
+			              </div>
+			            </div>		            
+			            <div className="open-search">
+			              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+			            </div>
+			          </div>
+		        	)}
+		    </div>
+		)
+	}	
+}
+
+export default BookList
